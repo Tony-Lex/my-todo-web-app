0943258bc22a27978fcef359afaf15bb71f61f38
TonyLex <tonylexmail@gmail.com>
2024-08-27 01:51:29 +0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """Function description goes here"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Function description goes here"""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
